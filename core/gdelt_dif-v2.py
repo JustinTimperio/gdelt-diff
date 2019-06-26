daa2757ef4f8161335bf267626a65c5b497c69b0
Justin Timperio <justin@epicycle.solutions>
2019-06-26 18:49:34 -0400

    Changes to be committed:         new file:   core/daemons/gdelt-dif.service         new file:   core/daemons/gdelt-dif.timer         new file:   core/daemons/gdelt-live.service         new file:   core/daemons/gdelt-live.timer         new file:   core/gdelt_dif-v2.py         new file:   core/gdelt_live.py         new file:   core/global_defuns.py

---
+++ core/gdelt_dif-v2.py
@@ -0,0 +1,205 @@
+#### Download, convert, and sort gdelt source files automatically
+## This script requires: gnu rsync, gnu parallel, linux unzip, gnu gzip, linux touch, python wget, python requests 
+import requests, re, shutil, wget, os, sys, argparse
+from global_defuns import *
+
+###############
+## Defs and Vars
+###########
+base_dir = '/var/app/gdelt-dif'
+ram_dir = '/tmp/gdelt-dif'
+
+def load_from_disk(file_path):
+    if not os.path.exists(file_path): 
+        try: shutil.copyfile(base_dir + str(file_path)[4:], file_path) 
+        except IOError: sys.exit('Critical Error Restoring ' + file_path + ' from the disk!')
+
+##############
+def gdelt_dif(lang, fzf_force=False):
+###########
+### Install dependinces and build app
+########
+    def fresh_install():
+        print('Starting Fresh Install....')
+        pacman_install('rsync parallel unzip gzip')
+        pip_install('wget requests')
+        homedir = input('Enter the FULL Path of the Existing ' + lang.upper() + ' Gdelt Repo (IE:/mnt/lt-mem/gdelt-v2/' + lang + '-stream):') 
+        mkdir(base_dir, 'u')
+        ## export path for stream
+        with open(base_dir + '/path-' + lang + '.txt', 'w') as f:
+            f.write(homedir)
+        mkdir(base_dir + '/' + lang + '-dl', 'u')
+        touch(base_dir + '/404-' + lang + '.txt', 'u')
+        ## search file system
+        print('Searching Through File System...')
+        fs = {os.path.join(dp, f) for dp, dn, fn in os.walk(os.path.expanduser(homedir)) for f in fn} 
+        fs = {path.replace(homedir,'') for path in fs}
+        fs = {path[9:-2] for path in fs}
+        fs = {"http://data.gdeltproject.org/gdeltv2/" + path + "zip" for path in fs}
+        export_list(base_dir + '/master-' + lang + '-previous.txt', fs) 
+
+    ## wait for user input if /var/tmp/gdelt-dif is missing
+    if not os.path.exists(base_dir + '/' + lang + '-dl'):
+        fresh = input('Persistence Files for ' + lang.upper() + ' are Missing! Do You Want to Perform a Fresh Install? (y/n):')
+        if fresh.lower() in ['y','yes']: 
+            fresh_install()
+        else:
+            return
+
+#############
+### Start Core Dif Process
+#########
+    ## restore all files to /tmp if missing
+    mkdir(ram_dir, 'u')
+    mkdir(ram_dir + '/' + lang + '-dl', 'u')
+    load_from_disk(ram_dir + '/master-' + lang + '-previous.txt')
+    load_from_disk(ram_dir + '/404-' + lang + '.txt')
+    load_from_disk(ram_dir + '/path-' + lang + '.txt')
+    ## download most recent masterfilelist.txt
+    ltmem = open(ram_dir + '/path-' + lang + '.txt').read()
+    if lang in 'english': 
+        dln = requests.get('http://data.gdeltproject.org/gdeltv2/masterfilelist.txt')
+    else: 
+        dln = requests.get('http://data.gdeltproject.org/gdeltv2/masterfilelist-translation.txt')
+    print(lang.upper() + ' Stream Status:', dln)
+    ## remove junk strings from list
+    rxhttp = re.compile("http*")
+    dln = list(filter(rxhttp.match,(dln.text.split())))
+    ## compare to previous dif
+    dlp = read_list(ram_dir + '/master-' + lang + '-previous.txt') 
+    dif = set(dln).difference(dlp)
+
+#############
+### Download Gdelt Files
+#########
+    def fetch():
+        fzfnew = set()
+        ## main download function
+        for url in dif:
+            try: wget.download(url,fetch_path) ## Download File URL
+            except: fzfnew.add(url) ## Or add to the 404 list on any error
+        export_list('/tmp/gdelt-dif/master-' + lang + '-previous.txt', dln)
+        export_list(base_dir + '/master-' + lang + '-previous.txt', dln)
+        #####
+        if fzf_force == True or len(fzfnew) >= 1:
+            ## retry any 404's 
+            print('\nRetrying Previous 404\'s...') 
+            fzfold = read_list('/tmp/gdelt-dif/404-' + lang + '.txt')
+            fzf = fzfold|fzfnew ## combine any old 404's with any new 404's
+            for url in fzf:
+                try: wget.download(url,fetch_path)
+                except: fzfnew.add(url)
+            print(len(fzfnew),'Total Files 404\'d')
+            export_list('/tmp/gdelt-dif/404-' + lang + '.txt', fzfnew) 
+            export_list(base_dir + '/404-' + lang + '.txt', fzfnew) 
+            
+#############
+### Convert Gdelt Source Files from .zip to .gz
+#########
+    def convert():
+        print('\nUnzipping Files...')
+        try: os.system('cd ' + fetch_path + ' && parallel unzip -qq ::: * && find . -iname "*.zip" -delete')
+        except: print('Unable to Decompress Files!') 
+        print('Compressing Files to .gz...')
+        try: os.system('cd ' + fetch_path + ' && parallel gzip ::: *')
+        except: print('Unable to Compress Files!') 
+
+#############
+## Sort Download Source Files into Folders
+#########
+    def sort():
+        dlfs = {os.path.join(dp, f) for dp, dn, fn in os.walk(os.path.expanduser(fetch_path)) for f in fn} 
+        if len(dlfs) == 0:
+            return  ## exit function if no files are found
+        if lang in 'translation':
+            offset = 12
+        else: offset = 0
+        gkgs = set()
+        for url in dif:
+            if len(url) == 63 + offset:
+                gkgs.add(url)
+        ## make year dirs
+        years = {"/" + path[37:-22 - offset] for path in gkgs}
+        for year in years:
+            if not os.path.exists(ltmem + year):
+                os.makedirs(ltmem + year)
+        ## make month dirs
+        months = {"/" + path[37:-22 - offset] + "/" + path[41:-20 - offset] for path in gkgs} 
+        for month in months:
+            if not os.path.exists(str(ltmem) + month):
+                os.makedirs(str(ltmem) + month)
+        ## move files to ltmem
+        print("Moving Files...")
+        for fil in dlfs:
+            if len(fil) == len(fetch_path) + 26 + offset:
+                os.system("rsync --remove-source-files " + fil + " " + ltmem + "/" + fil[len(fetch_path)+1:-21 - offset] + "/" + fil[len(fetch_path)+5:-19 - offset])
+            elif len(fil) == len(fetch_path) + 29 + offset:
+                os.system("rsync --remove-source-files " + fil + " " + ltmem + "/" + fil[len(fetch_path)+1:-24 - offset] + "/" + fil[len(fetch_path)+5:-22 - offset])
+            elif len(fil) == len(fetch_path) + 31 + offset:
+                os.system("rsync --remove-source-files " + fil + " " + ltmem + "/" + fil[len(fetch_path)+1:-26 - offset] + "/" + fil[len(fetch_path)+5:-24 - offset])
+            else:
+                print(str(len(fil)) + '=str_length :  File Name NOT Formated Correctly!')
+ 
+###############
+## Control Flow
+###############
+    print(str(len(dif)))
+    if len(dif) > 10000:
+        large_yn = input('More than 10k Files Are Missing! Do You Still Want to Continue? (y/n):')
+        if large_yn.lower() in ['y','yes']: 
+            fetch_path = str(base_dir + '/' + lang + '-dl/')
+            print('This May Take a While! Starting Download...')
+        else: sys.exit('Update Canceled!')
+    elif 10000 > len(dif) >= 1:
+        fetch_path = str(ram_dir + "/" + lang + '-dl/')
+    elif len(dif) == 0:
+        print('Source Files Up to Date!')
+        return
+    fetch()
+    convert()
+    sort()
+    print(lang.upper() + ' Dif Complete!')
+
+###############
+
+def remove():
+    if os.path.exists('/tmp/gdelt-dif'):
+        shutil.rmtree('/tmp/gdelt-dif')
+    if os.path.exists('/var/tmp/gdelt-dif'):
+        shutil.rmtree('/var/tmp/gdelt-dif')
+    sys.exit('All Files Removed!')
+
+###############
+
+parser = argparse.ArgumentParser(description="This script is used to automatically maintain a repository for the Gdelt Project. This script requires: gnu rsync, gnu parallel, linux unzip, gnu gzip, linux touch, linux find, python wget, python requests. Delete /var/tmp/gdelt-dif & /tmp/gdelt-dif to force a fresh install.")
+parser.add_argument("-d", "--dif", action='store_true', help="Dif and Download BOTH Gdelt Streams.")
+parser.add_argument("-de", "--dif_english", action='store_true', help="Dif and Download the Entire Gdelt English Stream.")
+parser.add_argument("-dt", "--dif_translation", action='store_true', help="Dif and Download the Entire Gdelt Translation Stream.")
+parser.add_argument("-r", "--retry", action='store_true', help="Force a 404-Retry on All Missing or Dead Files in BOTH Streams.")
+parser.add_argument("-re", "--retry_english", action='store_true', help="Force a 404-Retry on All Missing or Dead Files in English Stream.")
+parser.add_argument("-rt", "--retry_translation", action='store_true', help="Force a 404-Retry on All Missing or Dead Files in Translation Stream.")
+parser.add_argument("-rm", "--remove", action='store_true', help="Remove All Percestince Files. AKA-Uninstall")
+arguments = parser.parse_args()
+##
+if arguments.dif:
+    gdelt_dif('english')
+    print('------------------')
+    gdelt_dif('translation')
+elif arguments.dif_english:
+    gdelt_dif('english')
+elif arguments.dif_translation:
+    gdelt_dif('translation')
+##
+elif arguments.retry:
+    gdelt_dif('english')
+    print('------------------')
+    gdelt_dif('translation')
+elif arguments.retry_english:
+    retry('english')
+elif arguments.retry_translation:
+    retry('translation')
+##
+elif arguments.remove:
+    remove()
+else:
+    print('Missing Argument! Use --help to See All Arguments.')
